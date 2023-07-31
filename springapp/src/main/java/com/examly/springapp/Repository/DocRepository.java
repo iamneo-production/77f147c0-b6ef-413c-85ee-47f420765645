ecae90c71f0d34ea99340b13ecd64b39607d2b09
JeyaSundari060 <113575420+JeyaSundari060@users.noreply.github.com>
2023-07-31 22:08:53 +0530

    Update DocRepository

--- springapp/src/main/java/com/examly/springapp/Repository/DocRepository.java
+++ springapp/src/main/java/com/examly/springapp/Repository/DocRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.examly.springapp.Model.DocumentModel;
 
-@Repository
 public interface DocRepository extends JpaRepository<DocumentModel, String>{
-        Optional<DocumentModel> findPhotoById(long id);
-	DocumentModel findDocById(long id);
+    Optional<DocumentModel> findPhotoByDocid(String docid);
+
+	DocumentModel findDocByDocid(String docid);
 }
 
