34d388c9fe8bd51f86045ddea94caf90a3ef9c71
madumitha07 <118889957+madumitha07@users.noreply.github.com>
2023-07-31 19:45:43 +0530

    Update trackpage

--- angularapp/src/app/trackpage/trackpage.component.ts
+++ angularapp/src/app/trackpage/trackpage.component.ts
@@ -1,38 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ApplydocumentService } from '../userservice/applydocument.service';
-import { ApplyformService } from '../userservice/applyform.service';
+
+import { UpdateformService } from '../userservice/updateform.service';
+import { Applyform } from '../userservice/applyform';
 
 @Component({
-selector: 'app-trackpage',
-templateUrl: './trackpage.component.html',
-styleUrls: ['./trackpage.component.css']
+  selector: 'app-trackpage',
+  templateUrl: './trackpage.component.html',
+  styleUrls: ['./trackpage.component.css']
 })
-export class TrackpageComponent {
-id:number;
-Applieddocuments: Applyform=new Applyform ();
-setApprovalStatus : string;
-constructor(private route: ActivatedRoute,private router: Router,
-private newService: ApplyformService) { }
-ngOnInit() {
-const profileId = this.route.snapshot.params['id'];
-this.newService.getProfile(profileId).subscribe(
-(data) => {
-this.Applieddocuments = data;
-console.log(data.approvalStatus);
-},
-(error) => {
-console.log('Error fetching profile details:', error);
-}
-);
+export class TrackpageComponent implements OnInit {
 
-}
-viewAadhar(id: number){
-if(this.Applieddocuments.approvalStatus=="approved"){
-this.router.navigateByUrl(`/gaadhar/${id}`);
-}
-else if(this.Applieddocuments.approvalStatus=="rejected"){
-alert("Application has been rejected"); 
-} 
-}
+  id:number;
+  Applieddocuments: Applyform=new Applyform ();
+  setApprovalStatus : string;
+  constructor(private route: ActivatedRoute,private router: Router,
+  private newService: UpdateformService) { }
+  ngOnInit() {
+  const profileId = this.route.snapshot.params['id'];
+  this.newService.getProfile(profileId).subscribe(
+  (data) => {
+  this.Applieddocuments = data;
+  console.log(data.approvalStatus);
+  },
+  (error) => {
+  console.log('Error fetching profile details:', error);
+  }
+  );
+  
+  }
+  viewAadhar(id: number){
+  if(this.Applieddocuments.approvalStatus=="approved"){
+  this.router.navigateByUrl(`/gaadhar/${id}`);
+  }
+  else if(this.Applieddocuments.approvalStatus=="rejected"){
+  alert("Application has been rejected"); 
+  } 
+  }
+
 }
