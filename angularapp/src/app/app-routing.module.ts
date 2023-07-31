993fd0d93fc9579260ffe1b42ea1eac8548762d2
Hareni <118901278+Adol02@users.noreply.github.com>
2023-07-31 21:56:38 +0530

    Update app-routing.module.ts

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { CustomerapplyformComponent } from './customerapplyform/customerapplyform.component';
 import { CustomerapplydocumentComponent } from './customerapplydocument/customerapplydocument.component';
 import { CustomerapplyresponseComponent } from './customerapplyresponse/customerapplyresponse.component';
-import { AadharComponent } from './aadhar/aadhar.component';
-import { AdminhomeComponent } from './adminhome/adminhome.component';
-import { ApplieddocumentsComponent } from './applieddocuments/applieddocuments.component';
-import { CorrectiondocumentsComponent } from './correctiondocuments/correctiondocuments.component';
-import { CustomercorrectiondocumentComponent } from './customercorrectiondocument/customercorrectiondocument.component';
 import { CustomercorrectionformComponent } from './customercorrectionform/customercorrectionform.component';
-import { GenerateAadharComponent } from './generate-aadhar/generate-aadhar.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { CustomercorrectiondocumentComponent } from './customercorrectiondocument/customercorrectiondocument.component';
+import { ApplieddocumentsComponent } from './applieddocuments/applieddocuments.component';
+import { VerificationComponent } from './verification/verification.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { CorrectiondocumentsComponent } from './correctiondocuments/correctiondocuments.component';
+import { AadharComponent } from './aadhar/aadhar.component';
 import { TrackpageComponent } from './trackpage/trackpage.component';
 import { TrackstatusComponent } from './trackstatus/trackstatus.component';
-import { VerificationComponent } from './verification/verification.component';
+import { GenerateAadharComponent } from './generate-aadhar/generate-aadhar.component';
 
-  
 const routes: Routes = [
-  {path: '', component: LoginComponent},
+ {path: '', component: LoginComponent},
  {path: 'login', component: LoginComponent}, 
  {path: 'signup', component: SignupComponent},
  {path: 'home', component: HomeComponent},
@@ -36,7 +35,7 @@ const routes: Routes = [
  {path: 'track', component: TrackstatusComponent},
  {path: 'status/:id', component: TrackpageComponent},
  {path: 'gaadhar/:id', component: GenerateAadharComponent}
-];
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
