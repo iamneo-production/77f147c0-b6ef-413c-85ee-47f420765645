05b624fc21d6afe2143d7d44c0997ce89c41ba16
rithanya2903 <rithanyashree2903@gmail.com>
2023-07-31 16:21:26 +0000

    commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,22 +3,43 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { AadharComponent } from './aadhar/aadhar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerapplyformComponent } from './customerapplyform/customerapplyform.component';
+import { CustomerapplydocumentComponent } from './customerapplydocument/customerapplydocument.component';
+import { CustomerapplyresponseComponent } from './customerapplyresponse/customerapplyresponse.component';
+import { CustomercorrectionformComponent } from './customercorrectionform/customercorrectionform.component';
+import { CustomercorrectiondocumentComponent } from './customercorrectiondocument/customercorrectiondocument.component';
+import { ApplieddocumentsComponent } from './applieddocuments/applieddocuments.component';
+import { VerificationComponent } from './verification/verification.component';
 import { AdminhomeComponent } from './adminhome/adminhome.component';
-import { AdminnavComponent } from './adminnav/adminnav.component';
 import { CorrectiondocumentsComponent } from './correctiondocuments/correctiondocuments.component';
+import { AadharComponent } from './aadhar/aadhar.component';
+import { TrackpageComponent } from './trackpage/trackpage.component';
+import { TrackstatusComponent } from './trackstatus/trackstatus.component';
+import { GenerateAadharComponent } from './generate-aadhar/generate-aadhar.component';
 import { FormsModule } from '@angular/forms';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    CustomerapplyformComponent,
+    CustomerapplydocumentComponent,
+    CustomerapplyresponseComponent,
+    CustomercorrectionformComponent,
+    CustomercorrectiondocumentComponent,
+    ApplieddocumentsComponent,
+    VerificationComponent,
     AdminhomeComponent,
-    AdminnavComponent,
     CorrectiondocumentsComponent,
-    AadharComponent
-    
+    AadharComponent,
+    TrackstatusComponent,
+    TrackpageComponent,
+    GenerateAadharComponent
   ],
   imports: [
     BrowserModule,
