27cfb8b8382dbb91014bb5abd0386d2bf7ca57d6
swathijayaprakash01 <131763148+swathijayaprakash01@users.noreply.github.com>
2023-07-31 14:03:16 +0530

    Update app-routing.module

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
-
+import { GenerateAadharComponent } from './generate-aadhar/generate-aadhar.component';
 const routes: Routes = [
   {path: '', component: SignupComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'gaadhar/:id', component: GenerateAadharComponent},
 ];
 
 @NgModule({
