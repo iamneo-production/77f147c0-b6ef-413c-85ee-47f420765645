e20dccc1b89a9c7bf1d66188ad7c3d7b78dc622e
swathijayaprakash01 <swthijayaprakash@gmail.com>
2023-07-31 16:29:43 +0000

    changes

--- springapp/src/main/java/com/examly/springapp/Repository/AuthRepository.java
+++ springapp/src/main/java/com/examly/springapp/Repository/AuthRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import com.examly.springapp.model.UserModel;
+import com.examly.springapp.Model.UserModel;
 
 
 
