fdc932a1a7020d1786437813334fa8160c1113ec
Swetha-oss <116288826+Swetha-oss@users.noreply.github.com>
2023-07-31 21:50:57 +0530

    Update app-routing.module.ts

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CustomercorrectionformComponent } from './customercorrectionform/customercorrectionform.component';
-import { HomeComponent } from './home/home.component';
+import { CustomerapplyformComponent } from './customerapplyform/customerapplyform.component';
+import { CustomerapplydocumentComponent } from './customerapplydocument/customerapplydocument.component';
+import { CustomerapplyresponseComponent } from './customerapplyresponse/customerapplyresponse.component';
+import { AadharComponent } from './aadhar/aadhar.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { ApplieddocumentsComponent } from './applieddocuments/applieddocuments.component';
+import { CorrectiondocumentsComponent } from './correctiondocuments/correctiondocuments.component';
 import { CustomercorrectiondocumentComponent } from './customercorrectiondocument/customercorrectiondocument.component';
+import { CustomercorrectionformComponent } from './customercorrectionform/customercorrectionform.component';
+import { GenerateAadharComponent } from './generate-aadhar/generate-aadhar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TrackpageComponent } from './trackpage/trackpage.component';
+import { TrackstatusComponent } from './trackstatus/trackstatus.component';
+import { VerificationComponent } from './verification/verification.component';
 
+  
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'customercorrectionform', component: CustomercorrectionformComponent},
-  {path: 'Customercorrectiondocument', component: CustomercorrectiondocumentComponent}
+  {path: '', component: LoginComponent},
+ {path: 'login', component: LoginComponent}, 
+ {path: 'signup', component: SignupComponent},
+ {path: 'home', component: HomeComponent},
+ {path: 'applyform', component: CustomerapplyformComponent},
+ {path: 'applydoc/:email', component:  CustomerapplydocumentComponent},
+ {path: 'applyres/:email', component: CustomerapplyresponseComponent},
+ {path: 'customercorrectionform', component: CustomercorrectionformComponent},
+ {path: 'Customercorrectiondocument', component: CustomercorrectiondocumentComponent},
+ {path: 'appdoc', component:  ApplieddocumentsComponent},
+ {path: 'adminhome', component: AdminhomeComponent},
+ {path: 'corrdoc', component: CorrectiondocumentsComponent},
+ {path: 'aadhar/:id', component: AadharComponent},
+ {path: 'verify/:id', component: VerificationComponent},
+ {path: 'track', component: TrackstatusComponent},
+ {path: 'status/:id', component: TrackpageComponent},
+ {path: 'gaadhar/:id', component: GenerateAadharComponent}
 ];
 
 @NgModule({
